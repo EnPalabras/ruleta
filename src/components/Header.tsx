90036bcc1fa13f1038617cf00bcbe0034532425b
AgusIglesias72 <enpalabrass@gmail.com>
2023-10-17 17:03:12 -0300

    adadad

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,34 +26,6 @@ const ArrowLeft = ({ setPhase }: { setPhase: any }) => {
   )
 }
 
-const ShopCart = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="icon icon-tabler icon-tabler-shopping-cart
-        bg-[#b1b1d9]
-         h-12 w-12 md:h-14 md:w-14
-      rounded-md
-      mx-auto
-      p-1
-      cursor-pointer
-      "
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="black"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-      <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-      <path d="M17 17h-11v-14h-2"></path>
-      <path d="M6 5l14 1l-1 7h-13"></path>
-    </svg>
-  )
-}
-
 export default function Header({
   setPhase,
   setBgColor,
